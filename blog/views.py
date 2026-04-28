c1076a21f041b3deb68af81cc1d9353a80495350
CLiff <cliffnalldingvictor@gmail.com>
2026-04-28 18:09:09 -0400

    Finish Django blog locally

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
 from .models import Post
 
 def post_list(request):
-    post = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts: posts'})# Create your views here.
+    posts = Post.objects.all()
+
+    return render(request, "blog/post_list.html", {"posts": posts})
