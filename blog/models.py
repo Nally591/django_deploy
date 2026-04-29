60d101309e8a0fc0da8cf0b289b0c762c3ee228d
CLiff <cliffnalldingvictor@gmail.com>
2026-04-28 23:18:52 -0400

    Add CSS templates and forms

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,3 @@
-from django.db import models
-
-# Create your models here.
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -19,8 +16,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-from django.db import models
-
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
+
 
 def post_list(request):
     posts = Post.objects.all()
-
     return render(request, "blog/post_list.html", {"posts": posts})
+
+
+def post_new(request):
+    form = PostForm()
+    return render(request, "blog/post_edit.html", {"form": form})
